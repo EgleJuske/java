d6dd189d34e28ea55a8ec7dbf9b400a85b640c98
Eglė Juškevičienė <88egle@gmail.com>
2021-01-19 16:02:17 +0200

    files added

---
+++ 01-learning/src/Switch.java
@@ -0,0 +1,24 @@
+public class Switch {
+
+    public static void main(String[] args) {
+
+       int x = (int) Math.round(Math.random() * 10);
+        System.out.println(x);
+
+       switch (x) {
+           case 0:
+               System.out.println("nulis");
+               break;
+           default:
+               System.out.println("skaicius, kuris nera nulis, vienas arba du");
+               break;
+           case 1:
+               System.out.println("vienas");
+               break;
+           case 2:
+               System.out.println("du");
+               break;
+       }
+
+    }
+}
