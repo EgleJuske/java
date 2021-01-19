6cfc4aa221debc7ed00e2a308036f9f16ca910b1
Eglė Juškevičienė <88egle@gmail.com>
2021-01-19 18:20:40 +0200

    added exercise primarynum

---
+++ 01-learning/src/PrimaryNums.java
@@ -0,0 +1,21 @@
+public class PrimaryNums {
+
+    public static void main(String[] args) {
+
+        int start = 10;
+        int end = 91;
+
+        for (int j = start; j <= end; j++) {
+            int count = 0;
+            for (int i = 1; i <= j; i++) {
+                if (j % i == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.print(j + " ");
+            }
+        }
+
+    }
+}
